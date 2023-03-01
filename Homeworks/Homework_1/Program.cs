7382e8b368c735e96ce572f1be12b93f119492ea
Roman Kononov <djjem@MacBook-Pro-Roman.local>
2023-03-01 23:42:24 +0300

    Homewroks 1, 2

---
+++ Homeworks/Homework_1/Program.cs
@@ -0,0 +1,79 @@
+﻿//HomeWork_Task_2
+//Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее
+
+/*Console.WriteLine("Input number_1: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input number_2: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1 > number_2)
+{
+    Console.WriteLine("Max = " + number_1);
+    Console.WriteLine("Min = " + number_2);
+}
+if (number_1 < number_2)
+{
+    Console.WriteLine("Max = " + number_2);
+    Console.WriteLine("Min = " + number_1);
+}
+else
+{
+    Console.WriteLine("Числа равны");
+    Console.WriteLine("Max = Min = " + number_1);
+}*/
+
+
+
+
+//HomeWork_Task_4
+//Напишите программу, которая на вход принимает три числа и выдает максимальное из данных чисел
+
+/*Console.WriteLine("Input number_1: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input number_2: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input number_3: ");
+int number_3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number_1;
+
+if (number_2 > max)
+    max = number_2;
+if (number_3 > max)
+    max = number_3;
+
+Console.WriteLine("Max = " + max);*/
+
+
+
+
+//HomeWork_Task_6
+//Напишите программу, которая на вход принимает число и выдает является ли оно четным
+
+/*Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number%2 == 0)
+    Console.WriteLine("Чило четное!!!");
+else
+    Console.WriteLine("Чило нечетное!!!");*/
+
+
+
+
+//HomeWork_Task_8
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные число от 1 до N
+
+/*Console.WriteLine("Input N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int index = 2;
+
+while (index <= N)
+{
+    Console.Write(index + " ");
+    index += 2; 
+}*/
---
+++ Homeworks/Homework_2/Program.cs
@@ -0,0 +1,72 @@
+﻿ //HomeTask_10
+ //Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа
+
+ /*int SecondNumber (int num)
+ {
+    return num / 10 % 10;
+ }
+
+int number = 0;
+
+while (number < 99 || number > 999)
+{
+    Console.WriteLine("Введите трехзначное число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+ Console.WriteLine("Вторая цифра данного цисла = " + SecondNumber(number));*/
+
+
+
+
+ //HomeTask_13
+ //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+
+/*void ThirdNumber (int num)
+{
+    if (num > -100 && num < 100) 
+        Console.WriteLine("Третьей цифры в данном числе нет!");
+    else 
+    {
+        while (num > 999 || num < -999)
+        {
+            num = num / 10;
+        }
+
+        Console.WriteLine("Третья цифра данного числа = " + Math.Abs(num%10));    
+    }
+}
+
+    Console.WriteLine("Введите целое число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+ThirdNumber (number);*/
+
+
+
+
+//HomeTask_13
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+bool Weekend (int num)
+{
+    if (num == 6 || num == 7)
+        return true;
+    else
+        return false;
+}
+
+int number = 0;
+
+while (number < 1 || number > 7)
+{
+    Console.WriteLine("Введите порядковый номер дня недели (от 1 до 7 включительно): ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+if (Weekend(number)== true)
+    Console.WriteLine("Этот день - выходной день!");
+else
+    Console.WriteLine("Этот день - рабочий день!");
+
