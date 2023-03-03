c803fa968778b7c87b00d62b3aa0be375cb3afa5
Roman Kononov <djjem@MacBook-Pro-Roman.local>
2023-03-03 12:23:44 +0300

    Seminar_3 + Homework_3_correct

--- Seminars/Program.cs
+++ Seminars/Program.cs
@@ -116,5 +116,110 @@ Console.WriteLine(MaxNumber(randNumber));*/
 
 //                                    SEMINAR_3
 
-//Task_1
-//Напишите программу
+//Task_17
+//Напишите программу которая принимает на вход координаты точки (x, y), при чем x!=0, y!=0 и выдает номер четверти плоскости
+
+/*Console.Write("Введите координату X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.Write("Точка лежит в первой четверти");
+}
+else if (x < 0 && y > 0)
+{
+    Console.Write("Точка лежит во второй четверти");
+}
+else if (x < 0 && y < 0)
+{
+    Console.Write("Точка лежит в третьей четверти");
+}
+else if (x > 0 && y < 0)
+{
+    Console.Write("Точка лежит в четвертой четверти");
+}
+else
+{
+    Console.Write("Точка лежит на оси");
+}*/
+
+
+//**************************************************************************************************************************
+
+
+//Task_18
+// Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+
+/*Console.Write("Input quarter ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+switch (quarter){
+    case 1:
+    Console.WriteLine("x > 0 && y > 0");
+    break;
+    case 2:
+    Console.WriteLine("x < 0 && y > 0");
+    break;
+    case 3:
+    Console.WriteLine("x < 0 && y < 0");
+    break;
+    case 4:
+    Console.WriteLine("x > 0 && y < 0");
+    break;
+}*/
+
+
+//**************************************************************************************************************************
+
+
+//Task_21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+/*double Power(double a, double b){
+    return Math.Pow((a-b), 2);
+}
+
+Console.Write("Input X1 ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input X2 ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input Y1 ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input Y2 ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+double res = Math.Sqrt(Power(x1, x2) + Power(y1, y2));
+res = Math.Round(res, 3);
+Console.WriteLine($"Расстояние между точками {res}");*/
+
+//double res = Math.Pow(number, 2); // возведение в степень
+//double res = Math.Sqrt(number); // извлечение квадратного корня
+//res = Math.Round(res, 3); // округление
+
+
+//**************************************************************************************************************************
+
+
+//Task_22
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+/*int number = 0;
+while (number < 1){
+    Console.Write("Input number ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+int current = 1;
+while(current <= number){
+    Console.Write(Math.Pow(current, 2) + " ");
+    current++;
+}*/
+
+
+//*****************************************************************************************************************************
+//*****************************************************************************************************************************
+
+
+//                                    SEMINAR_4
+
